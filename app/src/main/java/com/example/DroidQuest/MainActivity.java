1b8bb9b23e10779cbfec8a0fad7dc3bbe278b690
surslav05 <surslav05@gmail.com>
2024-05-30 11:51:32 +0300

    glava4

---
+++ app/src/main/java/com/example/DroidQuest/MainActivity.java
@@ -0,0 +1,128 @@
+package com.example.DroidQuest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "QuestActivity";
+    private static final String KEY_INDEX = "index";
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() вызван");
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() вызван");
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() вызван");
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() вызван");
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() вызван");
+    }
+    public Button mTrueButton;
+    public Button mFalseButton;
+    private ImageButton mNextButton;
+    private ImageButton mBackButton;
+    private TextView mQuestionTextView;
+    private Question[] mQuestionBank = new Question[] {
+            new Question(R.string.question_android, true),
+            new Question(R.string.question_linear, false),
+            new Question(R.string.question_service, false),
+            new Question(R.string.question_res, true),
+            new Question(R.string.question_manifest, true),
+            new Question(R.string.question_1, true),
+            new Question(R.string.question_2, true),
+            new Question(R.string.question_3, false),
+            new Question(R.string.question_4, true),
+            new Question(R.string.question_5, true),
+    };
+    private int mCurrentIndex = 0;
+    private void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
+    }
+    private void checkAnswer(boolean userPressedTrue) {
+        boolean answerIsTrue =
+                mQuestionBank[mCurrentIndex].isAnswerTrue();
+        int messageResId = 0;
+        if (userPressedTrue == answerIsTrue) {
+            messageResId = R.string.correct_toast;
+        } else {
+            messageResId = R.string.incorrect_toast;
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) вызван");
+        setContentView(R.layout.activity_main);
+
+        mQuestionTextView =
+                (TextView)findViewById(R.id.question_text_view);
+
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+
+
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(true);
+            }
+        });
+        mFalseButton = (Button) findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(false);
+            }
+        });
+        mNextButton = (ImageButton)findViewById(R.id.next_button);
+        mBackButton = (ImageButton)findViewById(R.id.back_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+        mBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex - 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
+        updateQuestion();
+
+    }
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+    }
+}
